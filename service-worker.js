e558983e1ad831f6a51344a1fe931f563032bbed
CHBChan <four@umich.edu>
2023-03-16 15:20:12 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fdbaf72f33b8de6adffb27b1381eb56d.js"
+  "/CHBChan/The-Odin-Project-CV-Application/precache-manifest.64313c916d3da3a1e38a8a8045f0a91d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/CHBChan/The-Odin-Project-CV-Application/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
