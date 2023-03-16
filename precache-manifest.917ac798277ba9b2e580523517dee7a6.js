f3f5e5aa1a66f1a67e11e0b43e0dc718f118bd75
CHBChan <four@umich.edu>
2023-03-16 15:05:10 -0400

    Updates

---
+++ precache-manifest.917ac798277ba9b2e580523517dee7a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "387c1bfda1b000fc74c7",
+    "url": "/CHBChan/The-Odin-Project-CV-Application/static/css/main.92c8411b.chunk.css"
+  },
+  {
+    "revision": "387c1bfda1b000fc74c7",
+    "url": "/CHBChan/The-Odin-Project-CV-Application/static/js/main.6ee8b1b3.chunk.js"
+  },
+  {
+    "revision": "c68453aea333f45e8835",
+    "url": "/CHBChan/The-Odin-Project-CV-Application/static/js/runtime~main.a3bddf5b.js"
+  },
+  {
+    "revision": "cb2bee6d612cb4021fb2",
+    "url": "/CHBChan/The-Odin-Project-CV-Application/static/js/2.26fc4b20.chunk.js"
+  },
+  {
+    "revision": "6b4f5c7fcc373106b2b1dd6c708f8c69",
+    "url": "/CHBChan/The-Odin-Project-CV-Application/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1978fdcc5446baf9c204e7f78073020.js"
+  "/CHBChan/The-Odin-Project-CV-Application/precache-manifest.917ac798277ba9b2e580523517dee7a6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/CHBChan/The-Odin-Project-CV-Application/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
